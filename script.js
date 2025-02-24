0cf78743ba42bd024f459583cb193c536c9ef650
Deepak <deepakpopli002@gmail.com>
2025-02-24 15:17:49 +0530

    Added futuristic landing page with animations

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Animated Typing Effect
+    new Typed("#typed-text", {
+        strings: ["Deepak 👋", "a Robotics Engineer 🤖", "an IoT Developer 🌐", "a Drone Expert 🚀"],
+        typeSpeed: 50,
+        backSpeed: 30,
+        loop: true
+    });
+});
